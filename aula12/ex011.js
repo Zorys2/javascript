d1c4783bfc0facb11888157beb20f656a7b8a4a8
Richard Goulart <richardryugoulart@gmail.com>
2023-03-30 21:07:17 -0300

    sorry not sorry

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 19
+console.log(`Você tem ${idade} anos`)
+if(idade < 16){
+    console.log(`TÁ PITIQUIN É?`)
+} else if(idade < 18 || idade > 65) {
+    console.log('serase mama?')
+} else {
+    console.log('vai tomar no cu velho broxa')
+}
