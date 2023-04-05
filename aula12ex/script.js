487736f55018df63c5d0419b6def9041cc6e1173
Richard Goulart <richardryugoulart@gmail.com>
2023-04-05 14:12:10 -0300

    aprendizado bala

--- aula12ex/script.js
+++ aula12ex/script.js
@@ -1,18 +1,19 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
+    img.setAttribute('id', 'foto')
     var data = new Date()
     //var hora = data.getHours()
-    var hora = 19
+    var hora = 20
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
-        img.scr = 'manha.png'
+        img.setAttribute('src', 'manha.png')
         document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora <= 18) {
-        img.scr = 'tarde.png'
+        img.setAttribute('src', 'tarde.png')
         document.body.style.background = '#b9846f'
     } else {
-        img.scr = 'noite.png'
+        img.setAttribute('src', 'noite.png')
         document.body.style.background = '#515154'
     }
 }
