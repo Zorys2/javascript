7764fe0f280483fd0646dbada0276dfce26fae95
Richard Goulart <richardryugoulart@gmail.com>
2023-04-18 16:36:48 -0300

    eita

--- aula19/script.js
+++ aula19/script.js
@@ -1,5 +1,21 @@
-let num = document.getElementById ('txtnum')
+let num = document.getElementById ('fnum')
+let lista = document.getElementById ('flista')
+let res = document.querySelector('div#res')
+let valores = []
 
-function tabas() {
-    console.log(`Valor ${num} adicionado.`)
+
+function isNumero(n) {
+
+}
+
+function inLista(n) {
+    
+}
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value)) {
+
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
 }
