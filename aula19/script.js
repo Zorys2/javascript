676de757545e92589075ce53432fbb73aadfbb8d
Richard Goulart <richardryugoulart@gmail.com>
2023-04-18 16:40:30 -0300

    a

--- aula19/script.js
+++ aula19/script.js
@@ -5,11 +5,19 @@ let valores = []
 
 
 function isNumero(n) {
-
+    if (Number(n) >=1 && Number(n) <=100) {
+        return true
+    } else {
+        return false
+    }
 }
 
-function inLista(n) {
-    
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
 }
 
 function adicionar() {
